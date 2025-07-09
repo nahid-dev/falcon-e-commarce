3bc76d72e077b55be2830cc4cca1e00e8b1efa6d
nahid-dev <nahidv232@gmail.com>
2025-07-09 14:46:42 +0600

    Fix: fixed the blocked mixed content issue in production

--- common/helpers/APIKit.js
+++ common/helpers/APIKit.js
@@ -1,20 +1,20 @@
 const { client } = require("./HTTPKit");
 
 const APIKit = {
-    public: {
-        getProducts: () => {
-            const url = "/shop/products";
-            return client.get(url);
-        },
-        getProductDetails: (slug) => {
-            const url = `/product/${slug}`;
-            return client.get(url);
-        },
-        getCategories: () => {
-            const url = "/categories";
-            return client.get(url);
-        },
-    }
-}
+  public: {
+    getProducts: () => {
+      const url = "/api/v1/shop/products";
+      return client.get(url);
+    },
+    getProductDetails: (slug) => {
+      const url = `/api/v1/product/${slug}`;
+      return client.get(url);
+    },
+    getCategories: () => {
+      const url = "/api/v1/categories";
+      return client.get(url);
+    },
+  },
+};
 
 export default APIKit;
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "157.230.240.97"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://157.230.240.97:9999/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
